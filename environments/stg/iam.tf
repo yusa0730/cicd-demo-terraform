460aa91f6593e9169d9566a8a7518c1a3998808d
yusa0730 <isizawa5226@gmail.com>
2026-05-18 04:28:24 +0900

    fix: oidc iam

--- environments/stg/iam.tf
+++ environments/stg/iam.tf
@@ -21,9 +21,9 @@ resource "aws_iam_role" "terraform" {
           }
           StringLike = {
             "token.actions.githubusercontent.com:sub" = [
-              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/main",
-              "repo:${var.github_org}/${var.github_repo}:environment:${var.env}",
               "repo:${var.github_org}/${var.github_repo}:pull_request",
+              "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/stg",
+              "repo:${var.github_org}/${var.github_repo}:environment:${var.env}",
             ]
           }
         }
