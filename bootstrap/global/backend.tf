b7e264b8f84b489a9b087b8a1e8f1845e1dbf990
yusa0730 <isizawa5226@gmail.com>
2026-05-23 16:46:36 +0900

    remove deprecated bootstrap/global

--- bootstrap/global/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "cicd-demo-terraform-global"
-    key          = "bootstrap/global/terraform.tfstate"
-    region       = "ap-northeast-1"
-    use_lockfile = true
-  }
-}
