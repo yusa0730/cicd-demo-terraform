f0e705c9249894c2ff79f62b6d637fb233a03dcd
yusa0730 <isizawa5226@gmail.com>
2026-05-23 15:38:00 +0900

    fix: import

--- bootstrap/global/imports.tf
+++ bootstrap/global/imports.tf
@@ -1,6 +1,1 @@
-# If the GitHub OIDC Provider already exists in this AWS account, import it here.
-# Remove this file after the first successful apply.
-import {
-  to = aws_iam_openid_connect_provider.github
-  id = "arn:aws:iam::218317313594:oidc-provider/token.actions.githubusercontent.com"
-}
+# Import block removed — OIDC Provider will be created fresh by terraform apply.
