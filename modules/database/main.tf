946df8e0351181b6956d0ad7f3d03192643068a1
yusa0730 <isizawa5226@gmail.com>
2026-06-07 18:06:08 +0900

    fix: ci

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -29,13 +29,6 @@ resource "aws_security_group" "rds" {
   name        = "${var.name_prefix}-rds-sg"
   description = "Security Group to ${var.name_prefix}"
   vpc_id      = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_rds_cluster" "this" {
